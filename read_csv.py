aa6704c3db3e6d791b00ad93db5b1fef94adff37
Reece <ralexo99@gmail.com>
2020-10-02 20:59:03 -0400

    Added the ability to create a division customized to the enrolled athletes gender.

--- read_csv.py
+++ read_csv.py
@@ -2,10 +2,11 @@ import csv
 
 #Global const
 TIME_INDICATOR = "X"
-FIRST_EVENT_DIVISION = "12+yrs"
-SECOND_EVENT_DIVISION = "12+yrs"
-THIRD_EVENT_DIVISION = "12+yrs"
-FILE_NAME = "enroll12.csv"
+FIRST_EVENT_MALE_DIVISION = "6-8yrs (male)"
+FIRST_EVENT_FEMALE_DIVISION = "6-8yrs (female)"
+# SECOND_EVENT_MALE_DIVISION = "6-8yrs (male)"
+# SECOND_EVENT_FEMALE_DIVISION = "6-8yrs (female)"
+FILE_NAME = "enrolls.csv"
 
 #Reads in specific CSV file and returns three lists corresponding to whichever event an athlete is signed up for
 def read_csv():
@@ -13,35 +14,39 @@ def read_csv():
     first_event = []
     second_event = []
     third_event = []
+    student_gender = []
 
     #CSV file layout:
-    #id, full name, date time, date time
+    #id, full name, date time, gender(M/F), date time
     #One or both of the date times wil have an 'X' symbolizing that they are enrolled in that specific date time.
 
     #Desired CSV layout:
-    #first, last, division (time)
+    #first, last, division (age and gender)
 
     with open(FILE_NAME, 'r') as csvfile:
         filereader = csv.reader(csvfile, delimiter=',', quotechar='"', skipinitialspace=True)
 
         for row in filereader:
-            time_ind = row.index("X") - 1
+            time_ind = row.index("X") - 2
 
             #Add name to correct event list
             if(time_ind == 1):
                 first_event.append(row[1].split(sep=", "))
+                student_gender.extend([row[1].split(sep=", "), row[2]])
             if(time_ind == 2):
                 second_event.append(row[1].split(sep=", "))
+                student_gender.extend([row[1].split(sep=", "), row[2]])
             if(time_ind == 3):
                 third_event.append(row[1].split(sep=', '))
+                student_gender.extend([row[1].split(sep=", "), row[2]])
     
     #Reverse names to get first,last and add divisons
     rev_names(first_event)
-    add_div(first_event, FIRST_EVENT_DIVISION)
+    add_div(first_event, student_gender)
     rev_names(second_event)
-    add_div(second_event, SECOND_EVENT_DIVISION)
+    add_div(second_event, student_gender)
     rev_names(third_event)
-    add_div(third_event, THIRD_EVENT_DIVISION)
+    add_div(third_event, student_gender)
 
     return first_event, second_event, third_event
 
@@ -54,8 +59,20 @@ def rev_names(l):
 #Adds the 'Division' header to each row in list
 #PARAM: l - Given list
 #PARAM: s - String object to add to each element
-def add_div(l, s):
+def add_div(l, genderList):
     for row in l:
-        row.append(s)
+        gender = get_gender(row[0], genderList)
+        # print(gender)
+        # print(genderList[1])
+        if gender == "Female":
+            row.append(FIRST_EVENT_FEMALE_DIVISION)
+        else:
+            row.append(FIRST_EVENT_MALE_DIVISION)
+        
 
+def get_gender(el, genderList):
+    for i in genderList:
+        if el == i[1]:
+            return genderList[genderList.index(i)+1]
+        
 read_csv()
